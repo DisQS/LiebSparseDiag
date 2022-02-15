113021ccd16465560e6660335351ff838b95966e
Rudo Roemer <phsht@login01.avon.hpc>
2022-02-15 18:33:10 +0000

    updated to work for *-????.raw only

--- PBS/rename-MAS.sh
+++ PBS/rename-MAS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -name "*.raw" | awk -F'[.-]' -v mvCmd='mv -vu "%s" "%s"\n'     '{ num=sprintf("%05d", $(NF-1));
+find . -name "*-????.raw" | awk -F'[.-]' -v mvCmd='mv -vu "%s" "%s"\n'     '{ num=sprintf("%05d", $(NF-1));
        old=$0;
        sub(/[0-9]+'.raw'/,num".raw");
        printf (mvCmd,old,$0);
