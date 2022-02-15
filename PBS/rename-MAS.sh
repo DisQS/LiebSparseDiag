9cb2ab6918927b98212ee153de6a144e8c06d890
RudoRoemer <r.roemer@warwick.ac.uk>
2022-02-15 09:16:25 +0000

    rename script to move *-1234.raw files into *-01234.raw format

---
+++ PBS/rename-MAS.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+find . -name "*.raw" | awk -F'[.-]' -v mvCmd='mv -vu "%s" "%s"\n'     '{ num=sprintf("%05d", $(NF-1));
+       old=$0;
+       sub(/[0-9]+'.raw'/,num".raw");
+       printf (mvCmd,old,$0);
+     }' > rename-SLA.sh
+#cat rename-SLA.sh
+source ./rename-SLA.sh
+
